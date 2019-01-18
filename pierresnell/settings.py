6972be65c6bfb457c959581c3d9bb7d18d95c206
Pierre Snell <pierre.snell.1@ulaval.ca>
2019-01-17 20:47:39 -0500

    sdf

--- pierresnell/settings.py
+++ pierresnell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3e*j!vs%m_-0efmx8(wxmhhnuts+st79&2h*qo5005#o+rr-sb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.111']
+ALLOWED_HOSTS = ['192.168.0.111', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
